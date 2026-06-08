dcc06953bcc82db88457b6f090c57ce1f482bbe3
sparkmahadi <mahadi.development@gmail.com>
2026-06-08 17:59:04 +0600

    updated site build

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
       <footer className="bg-gray-700 text-white text-center py-4 mt-8">
-        © {new Date().getFullYear()} BaseFinder. All rights reserved.
+        © {new Date().getFullYear()} BaseFinder. All rights reserved. - Md. Mahadi Hasan
       </footer>
     );
   }
